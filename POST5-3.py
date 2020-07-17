a618be8dd7e73e6bf4d5a65ede397c318bfc29e0
deeplearningIndia1001 <68417974+deeplearningIndia1001@users.noreply.github.com>
2020-07-17 14:17:01 +0530

    Add files via upload

---
+++ POST5-3.py
@@ -0,0 +1,21 @@
+#I.G : @deeplearningindia
+############################# BAR PLOT ################################
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight') #Also Try using 'ggplot'
+
+x = [5,8,10,2,1] #DataPoints for Class 01
+y = [12,16,6,7,3]
+
+x2 = [6,9,11] #DataPoints for Class 02
+y2 = [6,15,7]
+
+plt.bar(x,y,linewidth=1 , label ="Class1")
+plt.barr(x2,y2,linewidth=1 , label ="Class2")
+
+plt.title('@deeplearningindia')
+plt.ylabel('Y axis')
+plt.xlabel('X axis')
+plt.legend()
+plt.show()
